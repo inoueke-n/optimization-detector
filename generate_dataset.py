3c775729c6e3f6e162522398f272421c05a9ad9e
Davide Pizzolotto <davide.pizzolotto@gmail.com>
2021-01-18 17:50:09 +0900

    Generate dataset script
    Downloading files

--- generate_dataset.py
+++ generate_dataset.py
@@ -1,8 +1,17 @@
 import argparse
+import hashlib
 import multiprocessing
 import os
 import shutil
+import sys
+import tempfile
+import tarfile
+import urllib.request
+import urllib.error
 from argparse import Namespace
+from typing import Dict, List
+
+from tqdm import tqdm
 
 
 class color:
@@ -19,19 +28,33 @@ class color:
 
 
 def println_ok():
-    print(color.BOLD + color.GREEN + "OK" + color.END)
+    """
+    Prints OK in bold green
+    """
+    print(color.BOLD + color.GREEN + "OK" + color.END, flush=True)
 
 
 def println_err():
-    print(color.BOLD + color.RED + "ERR" + color.END)
+    """
+    Prints ERR in bold red
+    """
+    print(color.BOLD + color.RED + "ERR" + color.END, flush=True)
 
 
 def println_warn(text: str):
-    print("[" + color.BOLD + color.YELLOW + "WARN" + color.END + f"]: {text}")
+    """
+    Prints WARN in yellow, and the warning message
+    """
+    print("[" + color.BOLD + color.YELLOW + "WARN" + color.END + f"]: {text}",
+          flush=True)
 
 
 def println_info(text: str):
-    print("[" + color.BOLD + color.CYAN + "INFO" + color.END + f"]: {text}")
+    """
+    Prints INFO in cyan, and the info message
+    """
+    print("[" + color.BOLD + color.CYAN + "INFO" + color.END + f"]: {text}",
+          flush=True)
 
 
 def getopt() -> Namespace:
@@ -63,7 +86,13 @@ def getopt() -> Namespace:
 
 
 def check_flags(args: Namespace) -> Namespace:
-    print("Checking args... ", end="")
+    """
+    Asserts that the parameters passed to the script are consistent and
+    respects the specification. Moreover checks that the compilers exists
+    and creates the output folder if not existing.
+    :param args arguments received from ArgumentParser
+    """
+    print("Checking args... ", end="", flush=True)
     info = []
     cc = list(filter(None, args.cc.split(":")))
     cxx = list(filter(None, args.cxx.split(":")))
@@ -93,6 +122,20 @@ def check_flags(args: Namespace) -> Namespace:
     if len(wrong_cc) > 0:
         println_err()
         print(f"Could not find the following compilers: {wrong_cc}")
+    if not os.path.exists(args.output):
+        try:
+            os.makedirs(args.output)
+            info.append("Created output directory")
+        except IOError as err:
+            println_err()
+            print(f"Could not create directory {args.output}:")
+            print(err)
+            exit(1)
+    elif not os.path.isdir(args.output) or not os.access(args.output, os.W_OK):
+        println_err()
+        print(f"Output directory {args.output} already exists and is not "
+              f"writable")
+        exit(1)
     println_ok()
     for msg in info:
         println_info(msg)
@@ -102,18 +145,26 @@ def check_flags(args: Namespace) -> Namespace:
     return args
 
 
-def which(cc, wrong_cc):
-    for i, exe in enumerate(cc):
+def which(exe: List[str], wrong_exe: List[str]):
+    """
+    Locate some executables. Given a list of executable, locates them by
+    looking in the path and replaces their name with the absolute path. If
+    the executable can not be located, it is appended to the `wrong_exe` list
+    """
+    for i, exe in enumerate(exe):
         if not os.path.isfile(exe) or not os.access(exe, os.X_OK):
             abs = shutil.which(exe)
             if abs is not None:
-                cc[i] = abs
+                exe[i] = abs
             else:
-                wrong_cc.append(exe)
+                wrong_exe.append(exe)
 
 
 def check_host_system():
-    print("Checking host system... ", end="")
+    """
+    Checks the presence of some programs required by the current script
+    """
+    print("Checking host system... ", end="", flush=True)
     set = {("bash", "bash"),
            ("bison", "bison"),
            ("bzip2", "bzip2"),
@@ -145,7 +196,103 @@ def check_host_system():
         println_ok()
 
 
+def check_hash(folder: str, md5s: Dict, warn: set):
+    """
+    Asserts that all files in the given folder have the MD5 hash passed in the
+    dictionary as {"filename","hash"}. The last parameter, will be used to
+    append warning messages
+    """
+    checked = set()
+    for file in os.listdir(folder):
+        filename = os.path.join(folder, file)
+        hash = hashlib.md5()
+        with open(filename, "rb") as f:
+            for chunk in iter(lambda: f.read(4096), b""):
+                hash.update(chunk)
+        hash = hash.hexdigest()
+        if file in md5s:
+            if md5s[file] == hash:
+                checked.add(file)
+            else:
+                println_err()
+                print(f"Wrong hash for downloaded file {file} (Expected "
+                      f"{md5s[file]}, got {hash})")
+                exit(1)
+        else:
+            warn.add(f"Foreign file in sources folder: {file}")
+    difference = set(md5s.keys()) - checked
+    if len(difference) > 0:
+        println_err()
+        print(f"Missing the following downloads (listed in the md5 "
+              f"files): {difference}")
+        exit(1)
+    else:
+        println_ok()
+
+
+def prepare_folder(args: Namespace) -> Namespace:
+    """
+    Downloads required files and compress them into an archive.
+    Then extracts the archive in a temporary folder. Returns the same
+    arguments as input, but with the extra temporary folder.
+    """
+    warn = set()
+    md5s = {"": ""}  # these avoids problems with newlines
+    with open("resources/md5sums", "r") as fp:
+        for line in fp.read().splitlines():
+            split = list(filter(None, line.split(" ")))
+            md5s[split[1]] = split[0]
+    md5s.pop("")
+    if not os.path.exists("resources/sources.tar"):
+        print("Retrieving software... ", end="", flush=True)
+        urls = {"": ""}
+        with open("resources/wget-list", "r") as fp:
+            for url in fp.read().splitlines():
+                urls[url.rsplit('/', 1)[-1]] = url
+        with open("resources/md5sums", "r") as fp:
+            for line in fp.read().splitlines():
+                split = list(filter(None, line.split(" ")))
+                md5s[split[1]] = split[0]
+        urls.pop("")
+        if len(urls) != len(md5s):
+            println_err()
+            print("Different numbers of downloads and md5s in resources")
+            exit(1)
+        else:
+            print("")
+        for key in tqdm(urls.keys(), file=sys.stdout):
+            try:
+                filename = os.path.join("resources/sources", key)
+                urllib.request.urlretrieve(urls[key], filename)
+            except urllib.error.HTTPError as e:
+                print(f"Failed to retrieve file {e.url}: {e.code} {e.msg}")
+                exit(1)
+        print("Checking downloaded files... ", end="", flush=True)
+        check_hash("resources/sources", md5s, warn)
+        print("Compressing... ", end="", flush=True)
+        # no compression, as this is an archive of archives
+        with tarfile.open("resources/sources.tar", "w") as tar:
+            tar.add("resources/sources", arcname="sources")
+        # delete original files, at this point they are not needed anymore
+        for file in os.listdir("resources/sources"):
+            if file in urls:
+                os.remove(os.path.join("resources/sources", file))
+        println_ok()
+    args.build_dir = tempfile.mkdtemp()
+    print("Decompressing... ", end="", flush=True)
+    with tarfile.open("resources/sources.tar", "r") as tar:
+        tar.extractall(args.build_dir)
+    println_ok()
+    print("Checking extracted files... ", end="", flush=True)
+    check_hash(args.build_dir + "/sources", md5s, warn)
+    for msg in warn:
+        println_warn(msg)
+    return args
+
+
 if __name__ == "__main__":
     args = getopt()
     args = check_flags(args)
     check_host_system()
+    args = prepare_folder(args)
+    shutil.rmtree(args.build_dir)
